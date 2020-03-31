101330e4af543f7facb9b1e10da4d7e0499601cb
arun <arunv04@gmail.com>
2020-03-31 13:15:26 +0530

    Updated Initial Source code

---
+++ src/main/java/com/customerportal/demo/CustomerportalApplication.java
@@ -0,0 +1,13 @@
+package com.customerportal.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerportalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerportalApplication.class, args);
+	}
+
+}
