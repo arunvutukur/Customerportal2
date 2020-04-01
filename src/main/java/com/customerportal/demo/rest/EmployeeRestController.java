0e157661467a76c2445af56be3f5511ca2f9f4fb
arun <arunv04@gmail.com>
2020-04-01 11:34:57 +0530

    Added functionallity in finding single id

--- src/main/java/com/customerportal/demo/rest/EmployeeRestController.java
+++ src/main/java/com/customerportal/demo/rest/EmployeeRestController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -38,6 +39,19 @@ public class EmployeeRestController {
 		//return employeeDAO.findAll();
 		return employeeService.findAll();
 	}
+	
+	@GetMapping("/employees/{employeeId}")
+	public Employee getEmployee(@PathVariable int employeeId) {
+		
+		Employee theEmployee =employeeService.findById(employeeId);
+		
+		if( theEmployee ==null) {
+			throw new RuntimeException("the employee is not found" + employeeId);
+		}
+		
+		return theEmployee;		
+	}
+	
 	
 	@GetMapping("/hello")
 	public String HelloWorld() {
