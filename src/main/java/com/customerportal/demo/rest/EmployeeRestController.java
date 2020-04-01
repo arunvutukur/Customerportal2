5786ba3588d1a9a4d6188183464f5b4f76958432
arun <arunv04@gmail.com>
2020-04-01 12:19:48 +0530

    added update mapping

--- src/main/java/com/customerportal/demo/rest/EmployeeRestController.java
+++ src/main/java/com/customerportal/demo/rest/EmployeeRestController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -50,6 +52,18 @@ public class EmployeeRestController {
 		}
 		
 		return theEmployee;		
+	}
+
+	@PostMapping("/employees")
+	public Employee addEmployee(@RequestBody Employee theEmployee) {
+		
+		
+		//this is to force save of new item...instead of update and also just set incase they pass any id
+		//in Json so set id to 0
+		theEmployee.setId(0);
+		employeeService.save(theEmployee);		
+		return theEmployee;
+		
 	}
 	
 	
