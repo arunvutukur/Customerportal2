376fb4585ddd83b88d27e6a6fccc2e2ec0eeab64
arun <arunv04@gmail.com>
2020-04-12 02:22:41 +0530

    Created UI template for list food items

--- src/main/java/com/customerportal/demo/rest/FitnessTrackerRestController.java
+++ src/main/java/com/customerportal/demo/rest/FitnessTrackerRestController.java
@@ -3,6 +3,7 @@ package com.customerportal.demo.rest;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.customerportal.demo.entity.Employee;
 import com.customerportal.demo.entity.fitness;
 import com.customerportal.demo.service.fitnessTrackerService;
 
@@ -31,35 +33,56 @@ public class FitnessTrackerRestController {
 		fitnessTrackerService =thefitnessTrackerService;
 	}
 	
-	@GetMapping("/food")
-	public List<fitness> findAll(){
-
-		return fitnessTrackerService.findAll();
-	}	
-	@GetMapping("/food/{foodId}")
-	public fitness getEmployee(@PathVariable int foodId) {
-		
-		fitness thefitness_tracker =fitnessTrackerService.findById(foodId);
-		
-		if( thefitness_tracker ==null) {
-			throw new RuntimeException("the employee is not found " + foodId);
-		}
-		
-		return thefitness_tracker;		
-	}
-
-
-	
-	
-	@GetMapping("/hello")
-	public String HelloWorld() {
-	
-		return "Hello World";
+	@GetMapping("/list")
+	public String listEmployees(Model theModel) {
+			
+			// get employees from db
+		List<fitness> thefitness = fitnessTrackerService.findAll();
+			
+			// add to the spring model
+		theModel.addAttribute("employees", thefitness);
+			
+		return "food/list-fooditems";
 	}
 	
-	@GetMapping("/")
-	public String index() {
-		return "Greetings from Spring Boot!";
-	}
-		
+	
+	
+	
+	
+	
+	
+	
+	
+	
+//	@GetMapping("/food")
+//	public List<fitness> findAll(){
+//
+//		return fitnessTrackerService.findAll();
+//	}	
+//	@GetMapping("/food/{foodId}")
+//	public fitness getEmployee(@PathVariable int foodId) {
+//		
+//		fitness thefitness_tracker =fitnessTrackerService.findById(foodId);
+//		
+//		if( thefitness_tracker ==null) {
+//			throw new RuntimeException("the employee is not found " + foodId);
+//		}
+//		
+//		return thefitness_tracker;		
+//	}
+//
+//
+//	
+//	
+//	@GetMapping("/hello")
+//	public String HelloWorld() {
+//	
+//		return "Hello World";
+//	}
+//	
+//	@GetMapping("/")
+//	public String index() {
+//		return "Greetings from Spring Boot!";
+//	}
+//		
 }
